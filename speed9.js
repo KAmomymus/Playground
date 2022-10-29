9667fe3e8d7172896016f238390f6122045eab63
Kartik Jain <karrtik112@gmail.com>
2022-10-30 00:11:20 +0530

    Update and rename speed8.js to speed9.js

--- speed9.js
+++ speed9.js
@@ -1,4 +1,7 @@
-import { Chart } from 'https://cdn.jsdelivr.net/npm/chart.js';
+const url = document.createElement('script');  
+url.setAttribute('src','https://cdn.jsdelivr.net/npm/chart.js');
+document.head.appendChild(url);
+
 const plugin = ({ widgets, simulator }) => {
 
 
@@ -39,3 +42,4 @@ const plugin = ({ widgets, simulator }) => {
     })
 };
 export default plugin;
+
