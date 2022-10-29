c9b00078ae7292d4bc7c2830416db6a566f4db43
Kartik Jain <karrtik112@gmail.com>
2022-10-29 23:33:38 +0530

    Update hello.js

--- route_planning/hello.js
+++ route_planning/hello.js
@@ -1,9 +1,11 @@
 var msg = "Hello";  
 const plugin = ({widgets, simulator}) => {
+  var msg = "Hello World";
   widgets.register("test_plan",function(box,msg){
     console.log(msg);
     console.log(box);
   });
+  
 }
 
 export default plugin
