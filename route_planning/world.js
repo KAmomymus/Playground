a8cb6a7c780b326ae0e9e8cac645e6b1cfbfc4ff
Kartik Jain <karrtik112@gmail.com>
2022-10-29 23:38:30 +0530

    Update world.js

--- route_planning/world.js
+++ route_planning/world.js
@@ -1,7 +1,7 @@
 var msg = "Hello";  
 const plugin = ({widgets, simulator}) => {
   var msg = "Hello World";
-  widgets.register("test_plan",function(box,msg){
+  widgets.register("test_plans",function(box,msg){
     console.log(msg);
     console.log(box);
   });
