4570206386d4e5806f22129dacd692ffa38ef8c5
Kartik Jain <karrtik112@gmail.com>
2022-10-30 00:31:56 +0530

    Update and rename speed9.js to speed10.js

--- speed10.js
+++ speed10.js
@@ -1,7 +1,4 @@
-const url = document.createElement('script');  
-url.setAttribute('src','https://cdn.jsdelivr.net/npm/chart.js');
-document.head.appendChild(url);
-
+import Chart from "chart.js";
 const plugin = ({ widgets, simulator }) => {
 
 
