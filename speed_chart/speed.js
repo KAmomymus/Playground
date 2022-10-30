cad2e7eae72854bc2325fc52a789ddd1929b8cd5
the-centinal <praveenkyadav02@gmail.com>
2022-10-30 10:57:35 +0530

    using npm

---
+++ speed_chart/speed.js
@@ -0,0 +1,47 @@
+
+import Chart from 'chart.js/auto';
+
+
+const plugin = ({ widgets, simulator }) => {
+
+    
+
+    widgets.register("graph", (box) => {
+        
+        box.injectHTML(` 
+        <div style="height: 400px; width: 400px">
+        <canvas id="myChart"></canvas>
+    </div>`)
+        const labels = [
+            'January',
+            'February',
+            'March',
+            'April',
+            'May',
+            'June',
+        ];
+
+        const data = {
+            labels: labels,
+            datasets: [{
+                label: 'Speed Chart',
+                backgroundColor: 'rgb(255, 99, 132)',
+                borderColor: 'rgb(255, 99, 132)',
+                data: [0, 10, 5, 2, 20, 30, 45],
+            }]
+        };
+        const config = {
+            type: 'line',
+            data: data,
+            options: {}
+        };
+        const myChart = new Chart(
+        document.getElementById('myChart'),
+            config);
+
+
+        console.log("ok");
+    })
+};
+export default plugin;
+
---
+++ speed_chart/url.js
@@ -0,0 +1,7 @@
+const url = document.createElement('script');  
+url.setAttribute('src','https://cdn.jsdelivr.net/npm/chart.js');
+document.head.appendChild(url);
+
+
+
+export default url 
