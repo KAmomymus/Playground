30b353d7a3a7d1607a320b21da1c9c872bc999aa
the-centinal <praveenkyadav02@gmail.com>
2022-10-30 12:27:30 +0530

    Update door_monitor.js

--- doors_monitoring/door_monitor.js
+++ doors_monitoring/door_monitor.js
@@ -8,99 +8,44 @@ const plugin = ({ widgets, simulator, vehicle }) => {
         statusDiv = div
 
         div.innerHTML = `
-        <style>
-        .doors {
-            height: 340px;
-            width: 320px;
-            /* border: 3px solid red; */
-            font-family: Verdana, sans-serif;
-            background-color: #e1e8ed;
-            /* margin: 45px auto; */
-        }
         
-        .heading {
-            text-align: center;
-            padding: 10px;
-        }
-        
-        .door-img {
-            display: flex;
-            justify-content: center;
-        }
-        
-        .door-img img {
-            height: 150px;
-        }
-        
-        .door-indicators {
-            display: flex;
-            justify-content: space-around;
-            margin: 30px;
-            font-size: 25px;
-            /* glowing */
-            border: none;
-            color: #8da44a;
-            /* text-shadow: 1px 5px 4px rgba(0, 0, 0, .3), 0 0 2px rgba(174, 189, 56, 0.6), 0 0 10px rgba(174, 189, 56, 0.6), 0 0 20px rgba(174, 189, 56, 0.6), 0 0 30px rgba(174, 189, 56, 0.6), 0 0 40px #aebd38, 0 0 70px #aebd38, 0 0 80px #aebd38, 0 0 100px #aebd38; */
-        }
-        
-        .door-indicators span {
-            animation: neon4 1.5s ease-in-out infinite alternate;
-        }
-        
-        @keyframes neon4 {
-            from {
-                text-shadow: 0 0 10px #fff, 0 0 20px #fff, 0 0 30px #fff, 0 0 40px #b6ff00, 0 0 70px #b6ff00, 0 0 80px #b6ff00, 0 0 100px #b6ff00, 0 0 150px #b6ff00;
-            }
-            to {
-                text-shadow: 0 0 5px #fff, 0 0 10px #fff, 0 0 15px #fff, 0 0 20px #b6ff00, 0 0 35px #b6ff00, 0 0 40px #b6ff00, 0 0 50px #b6ff00, 0 0 75px #b6ff00;
-            }
-        }
-        
-        #l-glow {
-            display: none;
-            background: inherit;
-            border: none;
-            cursor: pointer;
-        }
-        
-        #l-glow span {
-            font-family: Verdana, sans-serif;
-            font-size: large;
-        }
-        
-        #r-glow {
-            display: none;
-            background: inherit;
-            border: none;
-            cursor: pointer;
-        }
-        
-        #r-glow span {
-            font-family: Verdana, sans-serif;
-            font-size: large;
-        }    
-        </style>
-        <div class="doors">
+        <div class="doors" style="height: 340px; width: 320px; font-family: Verdana, sans-serif; margin: auto 0; background-color: #e1e8ed;">
 
-            <div class="heading">
+            <div class="heading" style="text-align: center;
+            padding: 10px;">
                 <h1>
                     DOOR STATUS
 
                 </h1>
             </div>
 
-            <div class="door-img">
-                <img src="assets/truck.png" alt="door">
+            <div class="door-img" style="display: flex;
+            justify-content: center;">
+                <img src="assets/truck.png" alt="door" style="height: 150px;">
             </div>
-            <div class="door-indicators">
+            <div class="door-indicators" style=" display: flex;
+            justify-content: space-around;
+            margin: 30px;
+            font-size: 25px;
+            
+            border: none;
+            color: #8da44a;">
 
-                <button id="l-glow" onclick=lCloseDoor()>
+                <button id="l-glow" onclick=lCloseDoor() style="display: none;
+                background: inherit;
+                border: none;
+                cursor: pointer;">
 
-                    <span> Left Door Open</span>
+                    <span style="animation: neon4 1.5s ease-in-out infinite alternate; font-family: Verdana, sans-serif;
+                    font-size: large;"> Left Door Open</span>
                 </button>
 
-                <button id="r-glow" onclick=rCloseDoor()>
-                    <span> Right Door Open</span>
+                <button id="r-glow" onclick=rCloseDoor() style="display: none;
+                background: inherit;
+                border: none;
+                cursor: pointer;">
+                    <span style="animation: neon4 1.5s ease-in-out infinite alternate; font-family: Verdana, sans-serif;
+                    font-size: large;"> Right Door Open</span>
                 </button>
             </div>
 
